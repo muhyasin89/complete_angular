4f5de60b6131544a1e694741d9a77d2846ed1f97
muhyasin89 <muhyasin89@gmail.com>
2023-04-08 00:40:19 +0700

    [backup]add backup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,34 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
+import { StoreModule } from "@ngrx/store";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+
 import { RecipeService } from "./recipes/recipe.service";
 import { AuthComponent } from "./auth/auth.component";
 import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
+import { shoppingListReducer } from "./shopping-list/store/shopping-list.reducer";
+import { RecipeModule } from "./recipes/recipe.module";
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+   
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
+   
     AuthComponent,
     LoadingSpinnerComponent,
   ],
@@ -41,8 +38,17 @@ import { AuthInterceptorService } from "./auth/auth-interceptor.service";
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     HttpClientModule,
     AppRoutingModule,
+    RecipeModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    })
   ],
   providers: [
     ShoppingListService,
